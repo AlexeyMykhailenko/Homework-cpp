c1e134bc199e92655d0ba9c12428c84ad8eb7b5f
Oleksii Mykhailenko <8993077@gmail.com>
2025-10-01 10:52:02 +0300

    updated last task

--- lucky_tickets.cpp
+++ lucky_tickets.cpp
@@ -1,32 +1,17 @@
 #include <iostream>
 
-int* arr(int x){
-   int *num = new int[6];
-   int y = 5;
-   while(x){
-      num[y--] = x%10;
-      x /= 10;
-   }
-
-   return num;
-}
-
 bool chck(int x) {
-   int *n = arr(x);
-
-   int pt1 = n[0] + n[1] + n[2];
-   int pt2 = n[3] + n[4] + n[5];
-
-   return (pt1 == pt2);
+    int pt1 = (x / 100000) % 10 + (x / 10000) % 10 + (x / 1000) % 10;
+    int pt2 = (x / 100) % 10 + (x / 10) % 10 + x % 10;
+    return pt1 == pt2;
 }
 
-int main(){
-   int  *n = arr(5);
-   const int lim = 1000000;
+int main() {
+    const int lim = 1000000;
 
-   for (int i = 0; i < lim; i++) {
-      if (chck(i)) {
-         std::cout << i << '\n';
-      }
-   }
+    for (int i = 0; i < lim; i++) {
+        if (chck(i)) {
+            std::cout << i << '\n';
+        }
+    }
 }
