7ce7e54301d5e40f7e2ffe2e0be9067d098084a0
Oleksii Mykhailenko <8993077@gmail.com>
2025-10-08 00:52:27 +0300

    updated to_string.cpp file - 4th task

--- to_string.cpp
+++ to_string.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
-#include <string>
-#include <format>
-using namespace std;
+// #include <string>
+// #include <format>
+// using namespace std;
+
+//int main(){
+//    int x;
+//    string s;
+//    cout << "Enter a number: ";
+//    cin >> x;
+
+//    s = to_string(x);
+
+//    cout << format("Number {} is now converted to string - {}", x, s) << endl;
+
+//    return 0;
+//}
+
+const unsigned int in_num = 2342352345;
 
 int main(){
-    int x;
-    string s;
-    cout << "Enter a number: ";
-    cin >> x;
-
-    s = to_string(x);
-
-    cout << format("Number {} is now converted to string - {}", x, s) << endl;
-
-    return 0;
+    std::cout << "In num: " << in_num << std::endl;
+    unsigned int current_num = in_num;
+    while (current_num > 0)
+    {
+        int last_digit = current_num % 10;
+        std::cout << last_digit << std::endl;
+        current_num /= 10;
+    }
+    
+    std::cout << current_num << std::endl;
 }
